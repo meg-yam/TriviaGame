962ec85479fbde74eb19e359addb4d729b3a5e6e
meg-yam <megyam95@gmail.com>
2019-04-05 19:23:59 -0400

    Added Questions to Button Click

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,19 +1,53 @@
 var questions = [
-    one = true,
-    two = false,
-    three = true,
-    four = false,
-    five = false,
-    six = true,
-    seven = true,
-]
+    one = "<div>Tigers are the largest species of wild cat.</div>",
+    two = "<div>Tigers only have stripes on their fur, not their skin.</div>",
+    three = "<div>No two tigers have the same stripes.</div>",
+    four = "<div>Tigers are terrible swimmers and drown all the time.</div>",
+    five = "<div>There are six species of tigers still alive today.</div>",
+    six = "<div>Tigers prefer to hunt at night.</div>",
+    seven = "<div>Ligers are actually real.</div>",
+];
 
-var time;
+var radio = [
+    rOne= "<form><input type='radio' name='cat'>True<input type='radio' name='cat'>False</form>",
+    rTwo="<form><input type='radio' name='fur'>True<input type='radio' name='fur'>False</form>",
+    rFour="<form><input type='radio' name='stripe'>True<input type='radio' name='stripe'>False</form>",
+    rFive="<form><input type='radio' name='swim'>True<input type='radio' name='swim'>False</form>",
+    rSix="<form><input type='radio' name='hunt'>True<input type='radio' name='hunt'>False</form>",
+    rSeven="<form><input type='radio' name='liger'>True<input type='radio' name='liger'>False</form>",
+];
 
-$(".butn").on("click", function() {
-    time = setTimeout(function() {
-        alert("Time's up!");
-    }, 3 * 1000);
 
+var time = 60;
+
+
+
+$(document).ready(function() {
+    $("#btn").click(function() {
+
+        $("#clock").html("<div>" + time + "</div>");
+        
+        $("#instructions").html(questions[0]);
+        $("#instructions").append(radio[0]);
+
+        $("#instructions").append(questions[1]);
+        $("#instructions").append(radio[1]);
+
+        $("#instructions").append(questions[2]);
+        $("#instructions").append(radio[2]);
+
+        $("#instructions").append(questions[3]);
+        $("#instructions").append(radio[3]);
+
+        $("#instructions").append(questions[4]);
+        $("#instructions").append(radio[4]);
+
+        $("#instructions").append(questions[6]);
+        $("#instructions").append(radio[6]);
+
+
+
+        $("#bottom").html("<button id='btn'>SUBMIT</button>");
+
+    });
 });
-  
