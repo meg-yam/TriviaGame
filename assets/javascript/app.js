dfa959cdd54577606b0984bce15b3f454c7c8b9d
meg-yam <megyam95@gmail.com>
2019-04-04 22:23:28 -0400

    Created Other Paged & Started JS

---
+++ assets/javascript/app.js
@@ -0,0 +1,27 @@
+var questions = [
+    one = true,
+    two = false,
+    three = true,
+    four = false,
+    five = false,
+    six = true,
+    seven = true,
+]
+
+$().ready(function() {
+
+    $("#btn").on("click", function() {
+   
+        
+   
+   
+   
+   
+   
+   
+   
+   
+   
+    });
+
+});   
